d3d04248ce4c456ffe9ad30a3b5b550359b300f8
Ankur Yadav <ankur.june1999@gmail.com>
2024-07-01 18:30:16 +0530

    changes made

--- src/app/page.js
+++ src/app/page.js
@@ -29,7 +29,7 @@ export default function Home() {
       </div>
 
       <div>
-        <h1>Hello World</h1>
+        <h1>Hello World i am learning nextjs</h1>
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
